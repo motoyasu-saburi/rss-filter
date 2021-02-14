43c51af77fbd4a3eb3994f49950243e2f629bac3
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-02-14 19:35:59 +0900

    add

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from http import client
 from typing import List, Optional
 
 import feedparser
@@ -11,25 +13,51 @@ class Cve:
 
 class RssCollector:
     whitelist: List[Optional[str]]  #
-    WHITELIST_DIR = ""  # TODO
+
+    WHITELIST_DIR = os.getcwd() + "/resource/whitelist.txt"
     RSS_URL = "https://nvd.nist.gov/feeds/xml/cve/misc/nvd-rss.xml"
+    SLACK_INCOMING_WEBHOOK_URL_PATH: str
 
     def __init__(self):
-        self.set_whitelist("")  # TODO
+        self.set_whitelist(self.WHITELIST_DIR)
+        # TODO change Env to Arg
+        self.SLACK_INCOMING_WEBHOOK_URL_PATH = os.environ.get("INCOMING_WEBHOOK_PATH")  # Do not include domain.
+        if self.SLACK_INCOMING_WEBHOOK_URL_PATH is None:
+            raise Exception("Environment variable ( 'INCOMING_WEBHOOK_PATH' )  is not set.")
+
+
+
+
 
     def set_whitelist(self, whitelist_dir: str) -> None:
+        def __filter_duplicate_and_invalid_value(wl: List[str]) -> List[str]:
+            filtered_invalid_list: List[str] = list(filter(lambda target: target != "", wl))
+            return list(sorted(set(filtered_invalid_list), key=filtered_invalid_list.index))  # filter duplicate value
+
         with open(whitelist_dir) as f:
             file_content = f.read()
-            self.whitelist = file_content.split("\n")
+            self.whitelist = __filter_duplicate_and_invalid_value(file_content.split("\n"))
+
 
     def exist_in_filter_list(self, summary: str, whitelists: List[Optional[str]]) -> bool:
+        lower_summary = summary.lower()
         for wl in whitelists:
-            if summary in wl:
+            if wl.lower() in lower_summary:
                 return True
         return False
 
-    def push_slack(self):
-        pass  # TODO
+    def push_cve_to_slack(self, payload: List[Cve]) -> int:
+        SLACK_DOMAIN = "hooks.slack.com"
+        WEB_HOOK_PATH = self.SLACK_INCOMING_WEBHOOK_URL_PATH
+        WEB_HOOK_METHOD = "POST"
+
+        httpclient = client.HTTPSConnection(SLACK_DOMAIN)
+        headers = {"Content-type": "application/json"}
+        body = "{'text': 'python request test'}"  # TODO replace filtered cve
+        httpclient.request(method=WEB_HOOK_METHOD, url=WEB_HOOK_PATH, headers=headers, body=body)
+        response_status: int = httpclient.getresponse().status
+        httpclient.close()
+        return response_status
 
     def main(self):
         full_cve_list = feedparser.parse(self.RSS_URL).entries
--- test_main.py
+++ test_main.py
@@ -1,3 +1,5 @@
+import os
+from typing import List
 from unittest import TestCase
 
 from main import RssCollector
@@ -6,4 +8,27 @@ from main import RssCollector
 class TestRssCollector(TestCase):
     def test_exist_in_filter_list(self):
         rc = RssCollector()
-        rc.exist_in_filter_list()
+
+        whitelist: List[str] = ["0000", "0000", "ABCDEF"]
+        self.assertTrue(rc.exist_in_filter_list("test abcdefg test", whitelist))
+        self.assertTrue(rc.exist_in_filter_list("ABCDEFG", whitelist))
+        self.assertTrue(rc.exist_in_filter_list("ABCDEFGHI", whitelist))
+        self.assertTrue(rc.exist_in_filter_list("123ABCDEFGHI", whitelist))
+
+        self.assertFalse(rc.exist_in_filter_list("", whitelist))
+        self.assertFalse(rc.exist_in_filter_list("123456", whitelist))
+
+    def test_set_whitelist(self):
+        rc = RssCollector()
+        whitelist_dir = os.getcwd() + "/test/test_whitelist.txt"
+        rc.set_whitelist(whitelist_dir)
+
+        expect: List[str] = ["ABCDEFG", "1234567"]
+        self.assertEqual(rc.whitelist, expect)
+
+    def test_push_slack(self):
+        pass
+        # Comment out, because it includes side effect (write slack channel)
+        # rc = RssCollector()
+        # rc.push_slack()
+        # self.assertEqual(rc.push_slack(), 200)
