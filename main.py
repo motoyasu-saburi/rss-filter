fea3fc5568cde0dbe5578432cec03fd483566cb3
motoyasu-saburi <kumagoro_alice@yahoo.co.jp>
2021-02-16 23:38:06 +0900

    add

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dataclasses import dataclass
 
 @dataclass
 class Cve:
+    title: str
     url: str
     description: str
 
@@ -21,13 +22,10 @@ class RssCollector:
     def __init__(self):
         self.set_whitelist(self.WHITELIST_DIR)
         # TODO change Env to Arg
-        self.SLACK_INCOMING_WEBHOOK_URL_PATH = os.environ.get("INCOMING_WEBHOOK_PATH")  # Do not include domain.
-        if self.SLACK_INCOMING_WEBHOOK_URL_PATH is None:
-            raise Exception("Environment variable ( 'INCOMING_WEBHOOK_PATH' )  is not set.")
-
-
-
-
+        self.SLACK_INCOMING_WEBHOOK_URL_PATH = ""
+        #self.SLACK_INCOMING_WEBHOOK_URL_PATH = os.environ.get("INCOMING_WEBHOOK_PATH")  # Do not include domain.
+        #if self.SLACK_INCOMING_WEBHOOK_URL_PATH is None:
+        #    raise Exception("Environment variable ( 'INCOMING_WEBHOOK_PATH' )  is not set.")
 
     def set_whitelist(self, whitelist_dir: str) -> None:
         def __filter_duplicate_and_invalid_value(wl: List[str]) -> List[str]:
@@ -46,24 +44,25 @@ class RssCollector:
                 return True
         return False
 
-    def push_cve_to_slack(self, payload: List[Cve]) -> int:
+    def push_cve_to_slack(self, cves: List[Cve]) -> int:
         SLACK_DOMAIN = "hooks.slack.com"
         WEB_HOOK_PATH = self.SLACK_INCOMING_WEBHOOK_URL_PATH
         WEB_HOOK_METHOD = "POST"
-
         httpclient = client.HTTPSConnection(SLACK_DOMAIN)
         headers = {"Content-type": "application/json"}
-        body = "{'text': 'python request test'}"  # TODO replace filtered cve
-        httpclient.request(method=WEB_HOOK_METHOD, url=WEB_HOOK_PATH, headers=headers, body=body)
-        response_status: int = httpclient.getresponse().status
-        httpclient.close()
-        return response_status
+
+        for cve in cves:
+            text_body = f"<{cve.url}|{cve.title}>\n{cve.description}"
+            body = '{"text": "' + text_body + '"}'
+            httpclient.request(method=WEB_HOOK_METHOD, url=WEB_HOOK_PATH, headers=headers, body=body)
+            httpclient.close()
+        return 200 # response_status
 
     def main(self):
         full_cve_list = feedparser.parse(self.RSS_URL).entries
-        cve_list: List[Cve] = list(map(lambda cve: Cve(url=cve.id, description=cve.summary), full_cve_list))
-        filtered_cve_list: List[Cve] = list(filter(lambda cve: self.exist_in_filter_list(cve.description, self.whitelist), cve_list))
-        print(filtered_cve_list)
+        cve_list: List[Cve] = list(map(lambda cve: Cve(title=cve.title, url=cve.id, description=cve.summary), full_cve_list))
+        filtered_cve_list: List[Cve] = list(filter(lambda cve: not self.exist_in_filter_list(cve.description, self.whitelist), cve_list))
+        self.push_cve_to_slack(filtered_cve_list)
 
 
 if __name__ == '__main__':
